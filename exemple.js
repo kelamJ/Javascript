f2a67762362b72f1f6220fc62021b1987f1a9384
kelamJ <malekjulien80@gmail.com>
2023-11-14 16:29:40 +0100

    Suite cours javascript

--- exemple.js
+++ exemple.js
@@ -1,15 +1,3 @@
-function prevoirAge() {
-
-    let age = prompt("Quel est votre âge ?");
-    // age = parseInt(age);
-    // age = parseFloat(age);
-    age = Number(age);
-
-    // nombre = 45;
-    // nombreEnString = nombre + "";
-    // nombreEnString = nombre.toString();
-    alert("Bientôt vous aurez " + (age + 1) + " ans");
-
+for (let i = 1; i < 5; i++) {
+    console.log('Je sais compter regarder ' + i);
 }
-
-prevoirAge();
